e10d31f1153f233a3c6b07429fbb4a6113ee5ee1
Shadman Rahman <shadmxn@gmail.com>
2024-06-18 10:50:05 -0400

    add ISS Spotter 1

---
+++ iss.js
@@ -0,0 +1,21 @@
+const needle = require('needle');
+
+const fetchMyIP = function(callback) {
+  needle.get('https://api.ipify.org?format=json', (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    const ip = body.ip;
+    callback(null, ip);
+  });
+};
+
+module.exports = { fetchMyIP };
